322055c94599e739ee3a782541293d7290c3f9c9
Jérémy <jeremy.barette@icloud.com>
2022-05-03 16:48:34 +0200

    Add files via upload

--- srcs/requirements/Wordpress/conf/config.php
+++ srcs/requirements/Wordpress/conf/config.php
@@ -30,8 +30,9 @@ define( 'DB_PASSWORD', getenv( 'MARIADB_PASSWORD' ) );
 /** MySQL hostname */
 define( 'DB_HOST', 'mariadb:3306' );
 
-define( 'WP_SITEURL', 'https://gscalisi.42Nice.fr');
-define( 'WP_HOME', 'https://gscalisi.42Nice.fr');
+define( 'WP_SITEURL', 'https://jbarette.42.fr');
+
+define( 'WP_HOME', 'https://jbarette.42.fr');
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
